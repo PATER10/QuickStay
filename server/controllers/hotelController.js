663c21a3fba84a4b8363d97988cb4d72e3b3b9ae
Patryk Kubik <pat.04@o2.pl>
2026-02-22 23:36:52 +0100

    Initial commit

---
+++ server/controllers/hotelController.js
@@ -0,0 +1,25 @@
+import Hotel from "../models/Hotel.js";
+import User from "../models/User.js";
+
+export const registerHotel = async (req, res) => {
+    try {
+        const {name, address, adress, contact, city} = req.body;
+        const hotelAddress = address || adress;
+        const owner = req.user._id;
+
+        // Check if User Already Registered
+        const hotel = await Hotel.findOne({owner});
+        if(hotel){
+            return res.json({success: false, message: "Hotel Already Registered"})
+        }
+
+        await Hotel.create({name, address: hotelAddress, contact, city, owner});
+
+        await User.findByIdAndUpdate(owner, {role: "hotelOwner"});
+
+        return res.json({success: true, message: "Hotel Registered Successfully"})
+
+    } catch (error) {
+        return res.json({success: false, message: error.message})
+    }
+}
